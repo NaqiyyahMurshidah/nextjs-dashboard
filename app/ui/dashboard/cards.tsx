d6f5f6e0de3a85556e4481c6be5ba56fd46919d5
NaqiyyahMurshidah <naqiyyahmurshidah1@gmail.com>
2025-02-15 23:53:43 +0800

    update fetching data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +15,7 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  //const totalPaidInvoices = await fetchCardData('totalPaidInvoices');
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
