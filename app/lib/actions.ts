cb80013478775bddab268a6cd36f7b45f31e211e
NaqiyyahMurshidah <naqiyyahmurshidah1@gmail.com>
2025-02-16 18:55:31 +0800

    mutate data
    updating the data ( edit , delete , create )

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
     date: z.string(),
 });
 const CreateInvoice = FormSchema.omit({id:true, date:true});
+const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
 export async function createInvoice(formData: FormData) {
 
@@ -24,6 +25,7 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get("amount"),
         status: formData.get("status"),
       });
+      
       const amountInCents = amount * 100; //store the amount in cents
       const date = new Date().toISOString().split('T')[0]; //store the date in ISO format "YYYY-MM-DD"
 
@@ -34,4 +36,28 @@ export async function createInvoice(formData: FormData) {
       revalidatePath('/dashboard/invoices'); //revalidate ( clear the cache and trigger new request to the server )
       redirect('/dashboard/invoices'); //redirect to the invoices page
 
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql `DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
